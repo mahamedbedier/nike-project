eca53a77b718507745f33bb949aa65cadc0b77ff
Mahamed Bedier <113152094+mahamedbedier@users.noreply.github.com>
2023-07-08 16:45:16 +0300

    Add files via upload

---
+++ essets/js/main.js
@@ -0,0 +1,45 @@
+let humburger = document.getElementById("humburger")
+let line1 = document.getElementById("line1")
+let line2 = document.getElementById("line2")
+let line3 = document.getElementById("line3")
+let navBar = document.getElementById("navBar")
+let homeNav = document.getElementById("homeNav")
+let menNav = document.getElementById("menNav")
+let womenNav = document.getElementById("womenNav")
+let kidsNav = document.getElementById("kidsNav")
+
+humburger.onclick = function () {
+    humburger.classList.toggle("activeH")
+    navBar.classList.toggle("active")
+    line1.classList.toggle("active1")
+    line2.classList.toggle("active2")
+    line3.classList.toggle("active3")
+}
+homeNav.onclick = function () {
+    humburger.classList.toggle("activeH")
+    navBar.classList.toggle("active")
+    line1.classList.toggle("active1")
+    line2.classList.toggle("active2")
+    line3.classList.toggle("active3")
+}
+menNav.onclick = function () {
+    humburger.classList.toggle("activeH")
+    navBar.classList.toggle("active")
+    line1.classList.toggle("active1")
+    line2.classList.toggle("active2")
+    line3.classList.toggle("active3")
+}
+womenNav.onclick = function () {
+    humburger.classList.toggle("activeH")
+    navBar.classList.toggle("active")
+    line1.classList.toggle("active1")
+    line2.classList.toggle("active2")
+    line3.classList.toggle("active3")
+}
+kidsNav.onclick = function () {
+    humburger.classList.toggle("activeH")
+    navBar.classList.toggle("active")
+    line1.classList.toggle("active1")
+    line2.classList.toggle("active2")
+    line3.classList.toggle("active3")
+}
